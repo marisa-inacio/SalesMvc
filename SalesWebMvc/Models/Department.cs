801fa7e0b632468094a7b5004cdc81652f720dca
marisa-inacio <marisa.vicente.inacio@hotmail.com>
2022-04-05 07:39:33 +0100

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,32 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //implementa a associacao do departament com o seller
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller (Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //calcular o total de vendas do departamento para o intervalo de datas. ver todos os vendedores do dpt e somar o total das suas vendas nesse periodo
+            //cada vendedor - chama o total de vendas daquele periodo e soma
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
